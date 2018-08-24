eca2a8096e5af7593859de4a2ed701c5e3e61971
Ilya Krasnov <krasnov@parma.ru>
2018-08-24 19:31:37 +0500

    first commit

---
+++ Facade/Program.cs
@@ -0,0 +1,18 @@
+﻿using DifficultLibrary;
+using FacadeObject;
+using System;
+
+namespace FacadeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj1 = new DifficultObjectA();
+            var obj2 = new DifficultObjectB();
+            var facade = new Facade(obj1, obj2);
+            facade.Operate();
+            Console.ReadLine();
+        }
+    }
+}
